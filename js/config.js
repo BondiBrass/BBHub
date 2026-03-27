c496b3be6a39ea494da1f378edaa2039703425ee
BondiBrass <bondibrass@gmail.com>
2026-03-28 00:39:38 +1100

    Update API_URL in config.js
    use cloudflare bbhub-proxy

--- js/config.js
+++ js/config.js
@@ -1,7 +1,8 @@
 // KEEP YOUR ORIGINAL CONFIG CONTENT HERE.
 // Replace this file with your real config.js after unzip.
+//  API_URL: "https://script.google.com/macros/s/AKfycbyKvIKKCgQYvv0fM6T4bX1hslKn3Xy4efkGSBm24pEmCZYPuRSqWt08TuwALkctJ-yJ/exec"
 export const BBHUB_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyKvIKKCgQYvv0fM6T4bX1hslKn3Xy4efkGSBm24pEmCZYPuRSqWt08TuwALkctJ-yJ/exec",
+  API_URL: "https://bbhub-proxy.bondibrass.workers.dev",
   APP_TITLE: "BBHub",
   CONTACT_EMAIL: "bondibrass@gmail.com"
 };
